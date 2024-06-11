e4f63d2b7ec4981e4247b1aab9e6887ed6170ea3
LiMar2022 <originalprogrammer2022>
2024-06-11 18:15:19 -0500

    html y js para get y delete de pacientes

---
+++ ClinicaOdontologica/src/main/resources/static/js/post_pacientes.js
@@ -0,0 +1,77 @@
+window.addEventListener('load', function () {
+
+    //Al cargar la pagina buscamos y obtenemos el formulario donde estarán
+    //los datos que el paciente ingresará
+    const formulario = document.querySelector('#add_new_paciente');
+
+    //Ante un submit del formulario se ejecutará la siguiente funcion
+    formulario.addEventListener('submit', function (event) {
+
+        //creamos un JSON que tendrá los datos del nuevo paciente
+        const formData = {
+            nombre: document.querySelector('#nombre').value,
+            apellido: document.querySelector('#apellido').value,
+            cedula: document.querySelector('#cedula').value,
+            fecha_ingreso: document.querySelector('#fecha_ingreso').value,
+            domicilio: document.querySelector('#domicilio').value,
+            email: document.querySelector('#email').value,
+
+        };
+        //invocamos utilizando la función fetch la API pacientes con el método POST que guardará
+        //el paciente que enviaremos en formato JSON
+        const url = '/pacientes';
+        const settings = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }
+
+        fetch(url, settings)
+            .then(response => response.json())
+            .then(data => {
+                //Si no hay ningun error se muestra un mensaje diciendo que el paciente
+                //se agrego bien
+                let successAlert = '<div class="alert alert-success alert-dismissible">' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong></strong> Paciente agregado </div>'
+
+                document.querySelector('#response').innerHTML = successAlert;
+                document.querySelector('#response').style.display = "block";
+                resetUploadForm();
+
+            })
+            .catch(error => {
+                //Si hay algun error se muestra un mensaje diciendo que el paciente
+                //no se pudo guardar y se intente nuevamente
+                let errorAlert = '<div class="alert alert-danger alert-dismissible">' +
+                    '<button type="button" class="close" data-dismiss="alert">&times;</button>' +
+                    '<strong> Error intente nuevamente</strong> </div>'
+
+                document.querySelector('#response').innerHTML = errorAlert;
+                document.querySelector('#response').style.display = "block";
+                //se dejan todos los campos vacíos por si se quiere ingresar otro paciente
+                resetUploadForm();})
+    });
+
+
+    function resetUploadForm(){
+        document.querySelector('#nombre').value = "";
+        document.querySelector('#apellido').value = "";
+        document.querySelector('#cedula').value = "";
+        document.querySelector('#fecha_ingreso').value = "";
+        document.querySelector('#domicilio').value = "";
+        document.querySelector('#email').value = "";
+
+    }
+
+    (function(){
+        let pathname = window.location.pathname;
+        if(pathname === "/"){
+            document.querySelector(".nav .nav-item a:first").addClass("active");
+        } else if (pathname == "/get_pacientes.html") {
+            document.querySelector(".nav .nav-item a:last").addClass("active");
+        }
+    })();
+});
